9f82e4de20704fc3885488d7ae2af7bb46d2b1aa
panda2k <thelowkeypanda@gmail.com>
2020-04-12 20:03:43 -0700

    basic discord bot

--- wakeupbot.py
+++ wakeupbot.py
@@ -1,11 +1,42 @@
 import os
 import discord
+from discord.ext import commands
+from subprocess import Popen
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='!')
+subprocess_list = []
 
-class CustomClient(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord')
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord')
 
-client = CustomClient()
-client.run(TOKEN)
+@bot.command(name='add-monitor')
+async def add_monitor(ctx, product_sku: str):
+    monitor_process = Popen(f'python -c "from yeezysupplymonitor import monitor_site; monitor_site(\\"{product_sku}\\", 5)"')
+    subprocess_list.append((product_sku, monitor_process))
+    await ctx.channel.send(f'Now monitoring {product_sku}')
+
+
+@bot.command(name='remove-monitor')
+async def remove_monitor(ctx, product_sku: str):
+    for monitor in subprocess_list:
+        if monitor[0] == product_sku:
+            monitor[1].kill()
+            subprocess_list.remove(monitor)
+            await ctx.channel.send(f'Stopped monitoring {product_sku}')
+            return
+    await ctx.channel.send('There is no process monitoring SKU ' + product_sku)
+    
+
+@bot.command(name='list-monitors')
+async def list_monitors(ctx):
+    skus = ""
+    for monitor in subprocess_list:
+        skus += monitor[0]
+    if len(skus) == 0:
+        await ctx.channel.send('Not currently monitoring any SKUs')
+    else:
+        await ctx.channel.send('Currently monitoring these SKUs: ' + skus)
+
+bot.run(TOKEN)
