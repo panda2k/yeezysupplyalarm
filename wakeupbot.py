dda192065cd0c5e9b3da370ca95b545444b6a24e
michael <littlwang@gmail.com>
2020-04-16 08:15:16 -0700

    fix method call

--- wakeupbot.py
+++ wakeupbot.py
@@ -13,7 +13,7 @@ async def on_ready():
 
 @bot.command(name='add-monitor')
 async def add_monitor(ctx, product_sku: str):
-    monitor_process = Popen(f'python -c "from yeezysupplymonitor import YeezySupplyMonitor; monitor = YeezySupplyMonitor(); monitor.monitor_site(\\"{product_sku}\\", 5)"', shell=True)
+    monitor_process = Popen(f'python -c "from yeezysupplymonitor import YeezySupplyMonitor; monitor = YeezySupplyMonitor(); monitor.monitor_site(\\"{product_sku}\\", 60)"', shell=True)
     subprocess_list.append((product_sku, monitor_process))
     await ctx.channel.send(f'Started monitor task with PID {monitor_process.pid}')
     await ctx.channel.send(f'Now monitoring {product_sku}')
