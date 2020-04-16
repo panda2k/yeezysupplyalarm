1e37e41c11727671b8ccba8198e4e28ea9787ca1
michael <littlwang@gmail.com>
2020-04-15 23:34:40 -0700

    add logging and make monitor an object

--- yeezysupplymonitor.py
+++ yeezysupplymonitor.py
@@ -1,31 +1,53 @@
+import os
+import logging
+from datetime import datetime
 import json
 import requests
-import googlehomecontroller
 import time
 
-def check_product_availbility(sku):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'} # required for headless chrome to work on yeezy supply
-    url = (f'https://www.yeezysupply.com/api/products/{sku}/availability')
-    response = requests.get(url, headers=headers)
-    if response.headers['content-type'].find('application/json') == -1: # check to make sure response is JSON
-        return False
-    product = json.loads(response.content)
-    if product['availability_status'] != 'PREVIEW': # If product is JSON, check product status
-        return True
-    else: 
-        return False
+log_path = os.path.join(os.getcwd(), "logs/")
+if os.path.isdir(log_path) == False:
+    os.mkdir(log_path)
+logging.basicConfig(
+    level=logging.DEBUG, 
+    filename=log_path + datetime.now().strftime("%Y:%m:%d:%H:%M:%S").replace(':', '-') + ".txt",
+    filemode='w',
+    format='%(asctime)s, %(levelname)s - %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S'
+)
 
-def monitor_site(sku, monitor_delay):
-    while check_product_availbility(sku) == False:
-        time.sleep(monitor_delay)
-    wakeup_alarm()
+import googlehomecontroller
 
-def wakeup_alarm():
-    googlehomecontroller.play_music('spotify:track:49fT6owWuknekShh9utsjv')
+
+class YeezySupplyMonitor:
+    def check_product_availbility(self, sku):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0'} # required for headless chrome to work on yeezy supply
+        url = (f'https://www.yeezysupply.com/api/products/{sku}/availability')
+        response = requests.get(url, headers=headers)
+        if response.headers['content-type'].find('application/json') == -1: # check to make sure response is JSON
+            logging.debug('Product not found')
+            return False
+        product = json.loads(response.content)
+        logging.debug(product)
+        if product['availability_status'] != 'PREVIEW': # If product is JSON, check product status
+            logging.debug('Product available')
+            return True
+        else: 
+            logging.debug('Product not available')
+            return False
+
+    def monitor_site(self, sku, monitor_delay):
+        while self.check_product_availbility(sku) == False:
+            time.sleep(monitor_delay)
+        logging.debug('Triggering alarm')
+        wakeup_alarm()
+
+    def wakeup_alarm(self):
+        googlehomecontroller.play_music('spotify:track:49fT6owWuknekShh9utsjv')
 
 def main():
-    print(check_product_availbility('FY5158'))
-    #check_product('H67799')
+    monitor = YeezySupplyMonitor()
+    #monitor.monitor_site('x', 5)
 
 if __name__ == "__main__":
     main()
